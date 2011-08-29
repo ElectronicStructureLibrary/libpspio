b1335e71f564014a53f0aa6bd5937515dd11c017
joseba <>
2011-08-29 13:52:05 +0000

    Intented

--- src/pspio.h
+++ src/pspio.h
@@ -2,14 +2,14 @@
 
 /**
  * Wave-equations 
-*/
+ */
 #define SCHRODINGER 1
 #define SCALAR_REL 2
 #define DIRAC 3
 
 /**
-* Pseudopotential generation schemes
-*/
+ * Pseudopotential generation schemes
+ */
 #define BHS 1
 #define HSC 2
 #define HAMANN 3
@@ -22,8 +22,8 @@
 #define RRKJ 10
 
 /**
-* Main structure for pseudopotential data
-*/
+ * Main structure for pseudopotential data
+ */
 struct psp_data_t {
   double z; /**< Atomic number */
   char *title; /**< descriptive string for content of file read in */
@@ -40,10 +40,9 @@ struct psp_data_t {
 };
 
 
-//Other imports from ape:
 /**
-* Mesh structure
-*/
+ * Mesh structure
+ */
 struct mesh_t{
   int type; /**< Type of mesh */
   int np; /**< Number of points in mesh */
@@ -52,24 +51,23 @@ struct mesh_t{
 };
 
 
-//Other imports from ape:
 /**
-* Potential structure
-*/
+ * Potential structure
+ */
 struct potential_t{
-  qn_t *qn;  /**< struct with quantum numbers n l j for the potential */
+  qn_t *qn; /**< struct with quantum numbers n l j for the potential */
   double psp[]; /**< pseudopotential, on a radial grid */
   gsl_spline *psp_spl; /**< spline type, for future interpolation before output to calling code */
 };
 
 /**
-*General information about the state
-*/
+ *General information about the state
+ */
 struct state_t{
-  qn_t *qn;  /**< quantum numbers n l j for this wavefunction */
-  double *occ;  /**< occupation of the electronic state */
+  qn_t *qn; /**< quantum numbers n l j for this wavefunction */
+  double *occ; /**< occupation of the electronic state */
   double *eigenval; /**< eigenvalue of electronic state*/
-  char *label;  /**< string describing the electronic state - eg 2s or 4d1.5  */
+  char *label; /**< string describing the electronic state - eg 2s or 4d1.5 */
   double *rc; /**< cutoff radii used for pseudopotential generation */
   /// The wavefunctions 
   int np; /**< number of points */
@@ -78,8 +76,8 @@ struct state_t{
 };
 
 /** 
-* The quantum numbers
-*/
+ * The quantum numbers
+ */
 struct qn_t{
   int n; /**< main quantum number */
   int l; /**< angular momentum quantum number */
@@ -88,10 +86,10 @@ struct qn_t{
 
 
 struct kb_projector_t {
-int l_local; /**< angular momentum channel of local potential */
+  int l_local; /**< angular momentum channel of local potential */
   potential_t *vlocal; /**< local potential */
   int nproj; /**< number of projectors below */
-  qn_t qn[];  /**< quantum numbers for present projector */
+  qn_t qn[]; /**< quantum numbers for present projector */
   double en[]; /**< Kleinman Bylander energies */
   double proj[][]; /**< Kleinman Bylander projectors */
   gsl_spline p_spl[]; /**< spline structures for interpolating projectors*/
