4297ce8c0b6e875304ca0eaad7fdef14cf95d325
joseba <>
2011-08-30 15:53:59 +0000

    Deleted unnecesary Doxygen comments. These hasn't have to be documented for Doxygen.

--- src/libxccodes.h
+++ src/libxccodes.h
@@ -2,12 +2,12 @@
 #define PSPIO_LIBXCCODES_H
 
 #ifdef HAVE_LIBXC
-#include <xc_funcs.h> /**< this should point to the locally installed version of libxc/include/xc_funcs.h */
+#include <xc_funcs.h> /* this should point to the locally installed version of libxc/include/xc_funcs.h */
 #else
-#include <xc_funcs.h> /**< imported from libxc into pspio package - may need to be updated periodically */
+#include <xc_funcs.h> /* imported from libxc into pspio package - may need to be updated periodically */
 #endif
 
-/**< addition to the present libxc standard - define value for no xc explicitly */
+/* addition to the present libxc standard - define value for no xc explicitly */
 #define XC_NONE 0
 
 #endif
