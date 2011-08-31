65187ddae192e859d8ba7cc40441d1b04ef3d33b
pouillon <>
2011-08-31 10:58:08 +0000

    * Had error handling macros used for states

--- src/pspio_state.c
+++ src/pspio_state.c
@@ -29,13 +29,9 @@
  **********************************************************************/
 
 int pspio_state_alloc(pspio_state_t *state) {
-  if ( state != NULL ) {
-    return PSPIO_ERROR;
-  }
+  ASSERT(state == NULL, PSPIO_ERROR)
   state = (pspio_state_t *)malloc(sizeof(state));
-  if ( state == NULL ) {
-    return PSPIO_ERROR;
-  }
+  HANDLE_FATAL_ERROR(state == NULL, PSPIO_ENOMEM)
 
   state->np = -1;
   state->eigenval = 0.0;
@@ -56,13 +52,9 @@ int pspio_state_copy(pspio_state_t *dst, pspio_state_t *src) {
     return PSPIO_ERROR;
   }
   dst = (pspio_state_t *)malloc(sizeof(dst));
-  if ( dst == NULL ) {
-    return PSPIO_ERROR;
-  }
+  HANDLE_FATAL_ERROR(dst == NULL, PSPIO_ENOMEM)
 
-  if ( (src->wf == NULL) || (dst->wf != NULL) ) {
-    return PSPIO_ERROR;
-  }
+  ASSERT((src->wf != NULL) && (dst->wf == NULL), PSPIO_ERROR)
   s = src->np * sizeof(double);
   dst->wf = (double *)malloc(s);
   memcpy(dst->wf, src->wf, s);
@@ -74,17 +66,13 @@ int pspio_state_copy(pspio_state_t *dst, pspio_state_t *src) {
   dst->occ = src->occ;
   dst->rc = src->rc;
 
-  if ( (src->label == NULL) || (dst->label != NULL) ) {
-    return PSPIO_ERROR;
-  }
+  ASSERT((src->label != NULL) && (dst->label == NULL), PSPIO_ERROR)
   s = strlen(src->label);
   dst->label = (char *)malloc(s+1);
   memcpy(dst->label, src->label, s);
   dst->label[s] = 0;
 
-  if ( (src->wfp == NULL) || (dst->wfp != NULL) ) {
-    return PSPIO_ERROR;
-  }
+  ASSERT((src->wfp != NULL) && (dst->wfp == NULL), PSPIO_ERROR)
   s = src->np * sizeof(double);
   dst->wfp = (double *)malloc(s);
   memcpy(dst->wfp, src->wfp, s);
@@ -107,35 +95,27 @@ int pspio_state_set(pspio_state_t *state, const int np, const double eigenval,
       const double *wfp) {
   int s;
 
-  if ( state == NULL ) {
-    return PSPIO_ERROR;
-  }
+  ASSERT(state != NULL, PSPIO_ERROR)
 
   state->np = np;
   state->eigenval = eigenval;
   state->occ = occ;
   state->rc = rc;
 
-  if ( (label == NULL) || (state->label != NULL) ) {
-    return PSPIO_ERROR;
-  }
+  ASSERT((label != NULL) && (state->label == NULL), PSPIO_ERROR)
   s = strlen(label);
   state->label = (char *)malloc(s + 1);
   memcpy(state->label,label,s);
   state->label[s] = 0;
 
-  if ( (wf == NULL) || (state->wf != NULL) ) {
-    return PSPIO_ERROR;
-  }
+  ASSERT((wf != NULL) && (state->wf == NULL), PSPIO_ERROR)
   state->wf = (double *)malloc(np);
   if ( state->wf == NULL ) {
     return PSPIO_ERROR;
   }
   memcpy(state->wf,wf,np);
 
-  if ( (wfp == NULL) || (state->wfp != NULL) ) {
-    return PSPIO_ERROR;
-  }
+  ASSERT((wfp != NULL) && (state->wfp == NULL), PSPIO_ERROR)
   state->wfp = (double *)malloc(np);
   if ( state->wfp == NULL ) {
     return PSPIO_ERROR;
