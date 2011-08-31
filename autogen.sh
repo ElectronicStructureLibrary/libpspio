7ed02e7aab09871ebb1f33b368f08aef3636ed32
pouillon <>
2011-08-31 18:11:24 +0000

    * Added missing copyright headers

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 #
-# Copyright (C) 2011 Yann Pouillon
+# Copyright (C) 2011 J. Alberdi, M. Oliveira, Y. Pouillon, and M. Verstraete
 #
-# This file is part of the Libpspio software package. For license information,
-# please see the COPYING file in the top-level directory of the source
-# distribution.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or 
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# $Id$
 #
 
 # Stop at first error encountered
