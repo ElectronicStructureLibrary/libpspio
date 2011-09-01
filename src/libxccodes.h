4733ed62b696b91800271ed195fc3aac9c7a45fe
pouillon <>
2011-09-01 15:07:08 +0000

    * Fixed spurious include of LibXC's header

--- src/libxccodes.h
+++ src/libxccodes.h
@@ -21,8 +21,6 @@
 #ifndef PSPIO_LIBXCCODES_H
 #define PSPIO_LIBXCCODES_H
 
-#include <xc_funcs.h>
-
 /* addition to the present libxc standard - define value for no xc explicitly */
 #define XC_NONE 0
 
