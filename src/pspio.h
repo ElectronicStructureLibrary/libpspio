71365e93699dd5b1174c34769241ccb48c65acb6
micael <>
2011-09-01 13:21:03 +0000

    * Some cleaning.  * Added psp_init interface.

--- src/pspio.h
+++ src/pspio.h
@@ -34,6 +34,10 @@
 #include "pspio_nlcc.h"
 
 
+/**********************************************************************
+ * Defines                                                            *
+ **********************************************************************/
+
 /**
  * Wave-equations 
  */
@@ -56,37 +60,60 @@
 #define RRKJ 10
 
 
+/**********************************************************************
+ * Data structures                                                    *
+ **********************************************************************/
+
 /**
  * Main structure for pseudopotential data
  */
 typedef struct{
   // general data
   char title[STRLEN_TITLE]; /**< descriptive string for content of file read in */
-  int wave_eq; /**< type of wave equation which was solved: Dirac, Scalar Relativisticof the License, or Schroedinger */
-  int exchange; /**< exchange functional id, taken from libxc conventions */
-  int correlation; /**< correlation functional id, taken from libxc conventions */
-  int scheme; /**< scheme used to generate the pseudopotentials */
-  int has_nlcc; /**< flag for presence of NLCC */
-  pspio_nlcc_t *nlcc; /**< NLCC structure */
-  pspio_mesh_t *mesh; /**< Radial mesh */
-
-  // atom data
-  double z; /**< Atomic number */
-  char *symbol; /**< Atomic symbol */
-
-  // valence data for psp
-  double zvalence; /**< charge of pseudopotential ion - valence electrons */
+  char *symbol;      /**< Atomic symbol */
+  double z;          /**< Atomic number */
+  double zvalence;   /**< charge of pseudopotential ion - valence electrons */
   double nelvalence; /**< number of electrons - normally equal to zion, except for special cases for ions */
-  int n_states; /**< number of electronic states */
+  int l_max;         /**< maximal angular momentum channel */
+  int wave_eq;       /**< type of wave equation which was solved: Dirac, Scalar Relativisticof the License, or Schroedinger */
+
+  // The radial mesh.
+  pspio_mesh_t *mesh; /**< Radial mesh - all functions should be discretized on this mesh */
+
+  // The states
+  int n_states;          /**< number of electronic states */
   pspio_state_t *states; /**< struct with electronic states */
-  int l_max; /**< maximal angular momentum channel */
-  int l_local; /**< angular momentum channel of local potential */
-  int n_potentials; /**< Number of pseudopotentials */
+
+  // The pseudopotentials
+  int scheme;                    /**< scheme used to generate the pseudopotentials */
+  int n_potentials;              /**< Number of pseudopotentials */
   pspio_potential_t *potentials; /**< struc with pseudopotentials */ 
-  int n_kbproj; /**< Number of Kleinman and Bylander projectors */
+
+  // Kleinman and Bylander non-local projectors
+  int n_kbproj;                     /**< Number of Kleinman and Bylander projectors */
   pspio_projector_t *kb_projectors; /**< Kleinman and Bylander projectors */
-  pspio_potential_t *vlocal; /**< local potential for the KB form of the pseudopotentials */ 
+  int l_local;                      /**< angular momentum channel of local potential */
+  pspio_potential_t *vlocal;        /**< local potential for the KB form of the pseudopotentials */ 
+
+  // Exchange and correlation data, including non-linear core corrections
+  int exchange;       /**< exchange functional id, taken from libxc conventions */
+  int correlation;    /**< correlation functional id, taken from libxc conventions */
+  int has_nlcc;       /**< flag for presence of NLCC */
+  pspio_nlcc_t *nlcc; /**< NLCC structure */
 
 } pspio_pspdata_t;
+
+
+/**********************************************************************
+ * Global routines                                                    *
+ **********************************************************************/
+
+/**
+ * 
+ * @param[in] file_name: file to be parsed.
+ * @param[in] file_format: the format of file_name.
+ * @param[in,out] psp_data: pointer to psp_data structure to be initialized
+ */
+int psp_init(char * file_name, int file_format, pspio_pspdata_t * psp_data);
 
 #endif
