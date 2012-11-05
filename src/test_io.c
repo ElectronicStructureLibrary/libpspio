5bdb1bf34337f469c609160d32ed68b105b24976
micael <>
2012-11-05 13:03:41 +0000

    * Renamed test_upf to test_io, as now it can be used to test any format.

--- src/test_io.c
+++ src/test_io.c
@@ -19,57 +19,57 @@
 */
 
 /**
- * @file test_upf.c
- * @brief checks pspio_upf.c and pspio_upf.h 
+ * @file test_io.c
+ * @brief checks functions that read and write the pseudopotentials
  */
 
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "upf.h"
 #include "pspio_pspdata.h"
 #include "test_common.h"
 
 
 int main(int argc, char *argv[]) {
-  int eid;
+  int eid, format;
   pspio_pspdata_t *pspdata = NULL;
 
-  if(argc != 3){
-    printf("Usage:\n%s input_file output_file\n", argv[0]);
+  if(argc != 4){
+    printf("Usage:\n%s input_file output_file format\n", argv[0]);
     return 1;
   }
+  format = atoi(argv[3]);
 
   /* Display basic information */
-  DEBUG_PRINT("%s - test_upf\nReport bugs to %s\n\n", PACKAGE_STRING,
+  DEBUG_PRINT("%s - test_io\nReport bugs to %s\n\n", PACKAGE_STRING,
     PACKAGE_BUGREPORT);
-  DEBUG_PRINT("=== BEGIN test_upf ===\n\n");
+  DEBUG_PRINT("=== BEGIN test_io ===\n\n");
 
   /* init pspdata */
-  DEBUG_PRINT("test_upf: initializing pspdata\n");
+  DEBUG_PRINT("test_io: initializing pspdata\n");
   eid = pspio_pspdata_init(&pspdata);
   eid = pspio_error_flush();
   DEBUG_PRINT("\n");
 
-  /* check parsing of UPF file */
-  DEBUG_PRINT("test_upf: parsing file %s\n", argv[1]);
-  eid = pspio_pspdata_read(&pspdata, argv[1], UPF);
+  /* check parsing of file */
+  DEBUG_PRINT("test_io: parsing file %s\n", argv[1]);
+  eid = pspio_pspdata_read(&pspdata, argv[1], format);
   eid = pspio_error_flush();
   DEBUG_PRINT("\n");
 
-  /* check writing of UPF file */
-  DEBUG_PRINT("test_upf: writing file %s\n", argv[2]);
-  eid = pspio_pspdata_write(pspdata, argv[2], UPF);
+  /* check writing of file */
+  DEBUG_PRINT("test_io: writing file %s\n", argv[2]);
+  eid = pspio_pspdata_write(pspdata, argv[2], format);
   eid = pspio_error_flush();
   DEBUG_PRINT("\n");
   
   /* Destroy pspdata structures */
-  DEBUG_PRINT("test_upf: destroying pspdata structure\n");
+  DEBUG_PRINT("test_io: destroying pspdata structure\n");
   eid = pspio_pspdata_free(&pspdata);
   eid = pspio_error_flush();
   DEBUG_PRINT("\n");
 
-  DEBUG_PRINT("=== END test_upf ===\n")
+  DEBUG_PRINT("=== END test_io ===\n")
 
   return 0;
 }
