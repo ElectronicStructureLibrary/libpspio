ccade00325c54e2f1889392d9b3ef3d28828bfac
pouillon <>
2012-11-05 20:03:14 +0000

    * Trivial fix: typos.

--- src/pspio_upf.c
+++ src/pspio_upf.c
@@ -35,7 +35,7 @@
 int pspio_upf_read(FILE *fp, pspio_pspdata_t **pspdata){
   int np, has_nlcc;
 
-  // Make sure we are at the beggining of the file
+  // Make sure we are at the beginning of the file
   rewind(fp);
 
   HANDLE_FUNC_ERROR(upf_read_info(fp, pspdata));
