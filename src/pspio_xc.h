45376ca2a41ad96a5955fa5c0d2ff94a3b5bfae0
micael <>
2012-11-08 15:27:41 +0000

    * Moved the content of pspio_xc_funcs.h to libxccodes.h.

--- src/pspio_xc.h
+++ src/pspio_xc.h
@@ -29,7 +29,7 @@
 #include "pspio_error.h"
 #include "pspio_mesh.h"
 #include "pspio_meshfunc.h"
-#include "pspio_xc_funcs.h"
+#include "libxccodes.h"
 
 /** 
  * values for NLCC scheme - could add possibilities for different schemes
