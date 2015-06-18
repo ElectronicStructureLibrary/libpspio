281f255dd68492f8957122cfd251e9f3f4c98f67
Micael Oliveira <micaeljtoliveira@gmail.com>
2015-06-18 10:36:32 +0200

    Initial implementation of unit tests using "check". For the moment only pspio_error is checked that way.

--- src/test_error.c
+++ src/test_error.c
@@ -1,5 +1,6 @@
 /*
  Copyright (C) 2011 J. Alberdi, M. Oliveira, Y. Pouillon, and M. Verstraete
+ Copyright (C) 2015 M. Oliveira
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License as published by
@@ -24,143 +25,145 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <check.h>
 
 #include "pspio_error.h"
-#include "test_common.h"
 
 
-int main(void) {
-  int eid = pspio_error_get_last(NULL);
+int eid;
 
-  /* Display basic information */
-  DEBUG_PRINT("%s - test_error\nReport bugs to %s\n\n", PACKAGE_STRING,
-    PACKAGE_BUGREPORT);
-  DEBUG_PRINT("=== BEGIN test_error ===\n\n");
-
-  /* check empty error chain */
-  DEBUG_PRINT("test_error: checking empty error chain\n");
-  DEBUG_PRINT("test_error: at the beginning, status = %d, length = %d\n",
-    eid, pspio_error_len());
-  CHECK_STAT_LEN(eid, PSPIO_SUCCESS, pspio_error_len(), 0);
-  DEBUG_PRINT("test_error: BEGIN FLUSH\n");
-  pspio_error_flush(stdout);
+void setup(void)
+{
   eid = pspio_error_get_last(NULL);
-  DEBUG_PRINT("test_error: END FLUSH\n");
-  DEBUG_PRINT("test_error: after pspio_error_flush, status = %d, length = %d\n",
-    eid, pspio_error_len());
-  CHECK_STAT_LEN(eid, PSPIO_SUCCESS, pspio_error_len(), 0);
-  DEBUG_PRINT("\n");
-
-  /* Check single error */
-  DEBUG_PRINT("test_error: checking single error (EVALUE)\n");
-  pspio_error_add(PSPIO_EVALUE, "test_1_1.c", 1234, "dummy1");
-  eid = pspio_error_get_last(NULL);
-  DEBUG_PRINT("test_error: after pspio_error_add, status = %d, length = %d\n",
-    eid, pspio_error_len());
-  CHECK_STAT_LEN(eid, PSPIO_EVALUE, pspio_error_len(), 1);
-  DEBUG_PRINT("test_error: BEGIN FLUSH\n");
-  pspio_error_flush(stdout);
-  eid = pspio_error_get_last(NULL);
-  DEBUG_PRINT("test_error: END FLUSH\n");
-  DEBUG_PRINT("test_error: after pspio_error_flush, status = %d, length = %d\n",
-    eid, pspio_error_len());
-  CHECK_STAT_LEN(eid, PSPIO_SUCCESS, pspio_error_len(), 0);
-  DEBUG_PRINT("\n");
-
-  /* Check double error */
-  DEBUG_PRINT("test_error: checking double error (EGSL, ENOSUPPORT)\n");
-  pspio_error_add(PSPIO_EGSL, "test_2_1.c", 201, "dummy21");
-  eid = pspio_error_get_last(NULL);
-  DEBUG_PRINT("test_error: after pspio_error_add, status = %d, length = %d\n",
-    eid, pspio_error_len());
-  CHECK_STAT_LEN(eid, PSPIO_EGSL, pspio_error_len(), 1);
-  pspio_error_add(PSPIO_ENOSUPPORT, "test_2_2.c", 202, "dummy22");
-  eid = pspio_error_get_last(NULL);
-  DEBUG_PRINT("test_error: after pspio_error_add, status = %d, length = %d\n",
-    eid, pspio_error_len());
-  CHECK_STAT_LEN(eid, PSPIO_ENOSUPPORT, pspio_error_len(), 2);
-  DEBUG_PRINT("test_error: BEGIN FLUSH\n");
-  pspio_error_flush(stdout);
-  eid = pspio_error_get_last(NULL);
-  DEBUG_PRINT("test_error: END FLUSH\n");
-  DEBUG_PRINT("test_error: after pspio_error_flush, status = %d, length = %d\n",
-    eid, pspio_error_len());
-  CHECK_STAT_LEN(eid, PSPIO_SUCCESS, pspio_error_len(), 0);
-  DEBUG_PRINT("\n");
-
-  /* Check triple error */
-  DEBUG_PRINT("test_error: checking triple error (EVALUE, ENOFILE, ERROR)\n");
-  pspio_error_add(PSPIO_EVALUE, "test_3_1.c", 311, "dummy31");
-  eid = pspio_error_get_last(NULL);
-  DEBUG_PRINT("test_error: after pspio_error_add, status = %d, length = %d\n",
-    eid, pspio_error_len());
-  CHECK_STAT_LEN(eid, PSPIO_EVALUE, pspio_error_len(), 1);
-  pspio_error_add(PSPIO_ENOFILE, "test_3_2.c", 322, "dummy32");
-  eid = pspio_error_get_last(NULL);
-  DEBUG_PRINT("test_error: after pspio_error_add, status = %d, length = %d\n",
-    eid, pspio_error_len());
-  CHECK_STAT_LEN(eid, PSPIO_ENOFILE, pspio_error_len(), 2);
-  pspio_error_add(PSPIO_ERROR, "test_3_3.c", 333, "dummy33");
-  eid = pspio_error_get_last(NULL);
-  DEBUG_PRINT("test_error: after pspio_error_add, status = %d, length = %d\n",
-    eid, pspio_error_len());
-  CHECK_STAT_LEN(eid, PSPIO_ERROR, pspio_error_len(), 3);
-  DEBUG_PRINT("test_error: BEGIN FLUSH\n");
-  pspio_error_flush(stdout);
-  eid = pspio_error_get_last(NULL);
-  DEBUG_PRINT("test_error: END FLUSH\n");
-  DEBUG_PRINT("test_error: after pspio_error_flush, status = %d, length = %d\n",
-    eid, pspio_error_len());
-  CHECK_STAT_LEN(eid, PSPIO_SUCCESS, pspio_error_len(), 0);
-  DEBUG_PRINT("\n");
-
-  /* Check whether pspio_error_get_last() works */
-  DEBUG_PRINT("test_error: checking pspio_error_get_last() on triple error\n");
-  pspio_error_add(PSPIO_EGSL, "test_4_1.c", 411, "dummy41");
-  eid = pspio_error_get_last(NULL);
-  DEBUG_PRINT("test_error: after pspio_error_add, status = %d, length = %d\n",
-    eid, pspio_error_len());
-  pspio_error_add(PSPIO_ENOFILE, "test_4_2.c", 422, "dummy42");
-  eid = pspio_error_get_last(NULL);
-  DEBUG_PRINT("test_error: after pspio_error_add, status = %d, length = %d\n",
-    eid, pspio_error_len());
-  pspio_error_add(PSPIO_ERROR, "test_4_3.c", 433, "dummy43");
-  eid = pspio_error_get_last(NULL);
-  DEBUG_PRINT("test_error: after pspio_error_add, status = %d, length = %d\n",
-    eid, pspio_error_len());
-  CHECK_STAT_LEN(eid, PSPIO_ERROR, pspio_error_len(), 3);
-  DEBUG_PRINT("test_error: pspio_error_get_last(dummy41) == EVALUE ?\n");
-  eid = pspio_error_get_last("dummy41");
-  DEBUG_PRINT("test_error: ---> %s\n", ( eid == PSPIO_EGSL ) ? "yes" : "no");
-  CHECK_STAT(eid, PSPIO_EGSL);
-  DEBUG_PRINT("test_error: pspio_error_get_last(dummy42) == ENOFILE ?\n");
-  eid = pspio_error_get_last("dummy42");
-  DEBUG_PRINT("test_error: ---> %s\n", ( eid == PSPIO_EGSL ) ? "yes" : "no");
-  CHECK_STAT(eid, PSPIO_ENOFILE);
-  DEBUG_PRINT("test_error: pspio_error_get_last(dummy43) == ERROR ?\n");
-  eid = pspio_error_get_last("dummy43");
-  DEBUG_PRINT("test_error: ---> %s\n", ( eid == PSPIO_EGSL ) ? "yes" : "no");
-  CHECK_STAT(eid, PSPIO_ERROR);
-  DEBUG_PRINT("test_error: pspio_error_get_last(dummy44) == SUCCESS ?\n");
-  eid = pspio_error_get_last("dummy44");
-  DEBUG_PRINT("test_error: ---> %s\n", ( eid == PSPIO_EGSL ) ? "yes" : "no");
-  CHECK_STAT(eid, PSPIO_SUCCESS);
-  DEBUG_PRINT("test_error: BEGIN FLUSH\n");
-  pspio_error_flush(stdout);
-  eid = pspio_error_get_last(NULL);
-  DEBUG_PRINT("test_error: END FLUSH\n");
-  DEBUG_PRINT("test_error: after pspio_error_flush, status = %d, length = %d\n",
-    eid, pspio_error_len());
-  CHECK_STAT_LEN(eid, PSPIO_SUCCESS, pspio_error_len(), 0);
-  DEBUG_PRINT("\n");
-
-  /* Destroy error-handling structures */
-  DEBUG_PRINT("test_error: destroying error-handling structures\n");
-  pspio_error_free();
-  CHECK_STAT_LEN(eid, PSPIO_SUCCESS, pspio_error_len(), 0);
-  DEBUG_PRINT("\n");
-
-  DEBUG_PRINT("=== END test_error ===\n");
-
-  return 0;
 }
+
+void teardown(void)
+{
+  pspio_error_free();
+}
+
+
+START_TEST(test_error_empty)
+{
+  ck_assert_int_eq(eid, PSPIO_SUCCESS);
+  ck_assert_int_eq(pspio_error_len(), 0);
+
+  pspio_error_flush(stdout);
+
+  ck_assert_int_eq(pspio_error_get_last(NULL), PSPIO_SUCCESS);
+  ck_assert_int_eq(pspio_error_len(), 0);
+
+}
+END_TEST
+
+START_TEST(test_error_single)
+{
+  pspio_error_add(PSPIO_EVALUE, "test_1_1.c", 1234, "dummy1");
+  ck_assert_int_eq(pspio_error_get_last(NULL), PSPIO_EVALUE);
+  ck_assert_int_eq(pspio_error_len(), 1);
+
+  pspio_error_flush(stdout);
+
+  ck_assert_int_eq(pspio_error_get_last(NULL), PSPIO_SUCCESS);
+  ck_assert_int_eq(pspio_error_len(), 0);
+}
+END_TEST
+
+START_TEST(test_error_double)
+{
+  pspio_error_add(PSPIO_EGSL, "test_2_1.c", 1234, "dummy21");
+  ck_assert_int_eq(pspio_error_get_last(NULL), PSPIO_EGSL);
+  ck_assert_int_eq(pspio_error_len(), 1);
+
+  pspio_error_add(PSPIO_ENOSUPPORT, "test_2_2.c", 202, "dummy22");
+  ck_assert_int_eq(pspio_error_get_last(NULL), PSPIO_ENOSUPPORT);
+  ck_assert_int_eq(pspio_error_len(), 2);
+
+  pspio_error_flush(stdout);
+
+  ck_assert_int_eq(pspio_error_get_last(NULL), PSPIO_SUCCESS);
+  ck_assert_int_eq(pspio_error_len(), 0);
+}
+END_TEST
+
+START_TEST(test_error_triple)
+{
+  pspio_error_add(PSPIO_EVALUE, "test_3_1.c", 311, "dummy31");
+  ck_assert_int_eq(pspio_error_get_last(NULL), PSPIO_EVALUE);
+  ck_assert_int_eq(pspio_error_len(), 1);
+
+  pspio_error_add(PSPIO_ENOFILE, "test_3_2.c", 322, "dummy32"); 
+  ck_assert_int_eq(pspio_error_get_last(NULL), PSPIO_ENOFILE);
+  ck_assert_int_eq(pspio_error_len(), 2);
+
+  pspio_error_add(PSPIO_ERROR, "test_3_3.c", 333, "dummy33"); 
+  ck_assert_int_eq(pspio_error_get_last(NULL), PSPIO_ERROR);
+  ck_assert_int_eq(pspio_error_len(), 3);
+
+  pspio_error_flush(stdout);
+
+  ck_assert_int_eq(pspio_error_get_last(NULL), PSPIO_SUCCESS);
+  ck_assert_int_eq(pspio_error_len(), 0);
+}
+END_TEST
+
+START_TEST(test_error_get_last)
+{
+  pspio_error_add(PSPIO_EGSL,    "test_4_1.c", 411, "dummy41");
+  pspio_error_add(PSPIO_ENOFILE, "test_4_2.c", 422, "dummy42");
+  pspio_error_add(PSPIO_ERROR,   "test_4_3.c", 433, "dummy43");
+  ck_assert_int_eq(pspio_error_len(), 3);
+
+  ck_assert_int_eq(pspio_error_get_last("dummy41"), PSPIO_EGSL);
+  ck_assert_int_eq(pspio_error_get_last("dummy42"), PSPIO_ENOFILE);
+  ck_assert_int_eq(pspio_error_get_last("dummy43"), PSPIO_ERROR);
+  ck_assert_int_eq(pspio_error_get_last("dummy44"), PSPIO_SUCCESS);
+}
+END_TEST
+
+
+Suite * make_error_suite(void)
+{
+  Suite *s;
+  TCase *tc_empty, *tc_single, *tc_double, *tc_triple, *tc_last;
+
+  s = suite_create("Error");
+
+  tc_empty = tcase_create("Empty chain");
+  tcase_add_test(tc_empty, test_error_empty);
+  suite_add_tcase(s, tc_empty);
+
+  tc_single = tcase_create("Single error chain");
+  tcase_add_test(tc_single, test_error_single);
+  suite_add_tcase(s, tc_single);
+
+  tc_double = tcase_create("Double error chain");
+  tcase_add_test(tc_double, test_error_double);
+  suite_add_tcase(s, tc_double);
+
+  tc_triple = tcase_create("Triple error chain");
+  tcase_add_test(tc_triple, test_error_triple);
+  suite_add_tcase(s, tc_triple);
+
+  tc_last = tcase_create("Get last");
+  tcase_add_test(tc_last, test_error_get_last);
+  suite_add_tcase(s, tc_last);
+    
+  return s;
+}
+
+int main(void)
+{
+  int number_failed;
+  Suite *s;
+  SRunner *sr;
+
+  s = make_error_suite();
+  sr = srunner_create(s);
+
+  srunner_run_all(sr, CK_VERBOSE);
+  number_failed = srunner_ntests_failed(sr);
+  srunner_free(sr);
+  return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
