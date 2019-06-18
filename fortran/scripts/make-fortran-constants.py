e338fec5daad24259a66dbd37fb174e6320cea99
Yann Pouillon <devops@materialsevolution.es>
2019-06-18 12:16:29 +0200

    Upgraded the Fortran constants maker script to Python 3

--- fortran/scripts/make-fortran-constants.py
+++ fortran/scripts/make-fortran-constants.py
@@ -21,39 +21,43 @@ and Fortran namespaces and avoid the inclusion of pspio_common.h in
 Fortran files.
 """
 
-__author__ = "Yann Pouillon"
-__copyright__ = "Copyright (C) 2014-2015 Yann Pouillon"
-__license__ = "LGPL version 3"
-__version__ = "0.1"
-
 import os
 import re
 
+
+__author__ = "Yann Pouillon"
+__copyright__ = "Copyright (C) 2014-2015 Yann Pouillon"
+__license__ = "LGPL version 3"
+__version__ = "0.2"
+
 # Init
-re_c_cmts  = re.compile("(/\*.*\*/|//.*)")
+re_c_cmts = re.compile("(/\*.*\*/|//.*)")
 re_cst_def = re.compile("^#define ")
-re_cst_f90 = re.compile("  !%%% BEGIN PSPIO CONSTANTS.*  !%%% END PSPIO CONSTANTS", flags=re.MULTILINE+re.DOTALL)
+re_cst_f90 = re.compile(
+    "  !%%% BEGIN PSPIO CONSTANTS.*  !%%% END PSPIO CONSTANTS",
+    flags=re.MULTILINE+re.DOTALL)
 
 # Translate C definitions into Fortran
 f90_defs = "  !%%% BEGIN PSPIO CONSTANTS\n"
 for line in file("../src/pspio_common.h", "r").readlines():
-  if ( re_cst_def.match(line) ):
-    line = re.sub(re_c_cmts, "", line)
-    line = line.split()
-    cst_name = line[1]
-    cst_value = " ".join(line[2:])
-    if ( cst_value != "" ):
-      try:
-        cst_value = int(cst_value)
-        f90_defs += "  integer(c_int), parameter, public :: %s = %d\n" % \
-          (cst_name, cst_value)
-      except:
-        # The following should be change to use the iso_c_bindings
-        f90_defs += "  character(len=*), parameter, public :: %s = %s\n" % \
-          (cst_name, cst_value)
+    if ( re_cst_def.match(line) ):
+        line = re.sub(re_c_cmts, "", line)
+        line = line.split()
+        cst_name = line[1]
+        cst_value = " ".join(line[2:])
+        if ( cst_value != "" ):
+            try:
+                cst_value = int(cst_value)
+                f90_defs += "  integer(c_int), parameter, public :: {name} = {value:d}\n".format(name=cst_name, value=cst_value)
+            except:
+                # The following should be changed to use iso_c_binding
+                f90_defs += "  character(len=*), parameter, public :: {name} = {value}\n".format(name=cst_name, value=cst_value)
 f90_defs += "  !%%% END PSPIO CONSTANTS"
 
 # Replace existing Fortran definitions
-f90_file = "src/pspiof.F90"
-f90_src  = file(f90_file, "r").read()
-file(f90_file, "w").write(re.sub(re_cst_f90, f90_defs, f90_src))
+f90_path = "src/pspiof.F90"
+with open(f90_path, "r+") as f90_file:
+    f90_src = f90_file.read()
+    f90_file.seek(0)
+    f90_file.write(re.sub(re_cst_f90, f90_defs, f90_src))
+    f90_file.truncate()
